b1e20fe1f06e865cdc87b81203ca6829ce7c81c3
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2021-01-13 16:33:25 +1300

    FIX: missing brackets meant it logged even if not actually bypassed

--- src/main/java/net/gabbage/playerslotbypass/PlayerSlotBypass.java
+++ src/main/java/net/gabbage/playerslotbypass/PlayerSlotBypass.java
@@ -18,10 +18,11 @@ public final class PlayerSlotBypass extends JavaPlugin implements Listener {
         // Checking if the reason we are being kicked is a full server
         if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
             // Checking if the player has the specified permission node
-            if (event.getPlayer().hasPermission("playerslot.bypass"))
+            if (event.getPlayer().hasPermission("playerslot.bypass")) {
                 // If the condition above is true, we execute the following code, that allows the player on the server
                 event.allow();
                 getLogger().info(event.getPlayer().getDisplayName() + " bypassed player slot limit");
+            }
         }
 
     }
