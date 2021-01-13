f811b0e537b9bb97326d7fad7b35abf15ff43595
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2021-01-13 16:45:00 +1300

    changed to google formatting

--- src/main/java/net/gabbage/playerslotbypass/PlayerSlotBypass.java
+++ src/main/java/net/gabbage/playerslotbypass/PlayerSlotBypass.java
@@ -7,23 +7,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerSlotBypass extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+  @Override
+  public void onEnable() {
+    getServer().getPluginManager().registerEvents(this, this);
+  }
 
+  @EventHandler
+  public void onPlayerLoginEvent(PlayerLoginEvent event) {
+    // Checking if the reason we are being kicked is a full server
+    if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
+      // Checking if the player has the specified permission node
+      if (event.getPlayer().hasPermission("playerslot.bypass")) {
+        // If the condition above is true, we execute the following code, that allows the player on
+        // the server
+        event.allow();
+        getLogger().info(event.getPlayer().getDisplayName() + " bypassed player slot limit");
+      }
     }
-
-    @EventHandler
-    public void onPlayerLoginEvent(PlayerLoginEvent event) {
-        // Checking if the reason we are being kicked is a full server
-        if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
-            // Checking if the player has the specified permission node
-            if (event.getPlayer().hasPermission("playerslot.bypass")) {
-                // If the condition above is true, we execute the following code, that allows the player on the server
-                event.allow();
-                getLogger().info(event.getPlayer().getDisplayName() + " bypassed player slot limit");
-            }
-        }
-
-    }
+  }
 }
